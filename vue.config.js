f23775e92b0a7bb4050db97ac8a6457e731427aa
Steven Atkinson <steven.atkinson@gmail.com>
2019-09-20 17:08:43 -0700

    Stop caching on your development server

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,19 @@
 // vue.config.js
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === 'production' ? '/' : '/GreaterGoodsSession'
+    process.env.NODE_ENV === 'production'
+      ? '/GreaterGoodsGrocers'
+      : '/GreaterGoodsGrocers',
+
+  configureWebpack: {
+    devServer: {
+      headers: {
+        'Cache-Control':
+          'must-revalidate, no-cache, no-store, no-transform, prive, proxy-revalidate, max-age=5',
+        'X-Frame-Options': 'DENY',
+        'X-XSS-Protection': '1; mode=block',
+        'X-Content-Type': 'nosniff'
+      }
+    }
+  }
 }
