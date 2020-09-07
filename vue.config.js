7b180203e0fe00e15eb703ac9b2baa248b729aed
Steven Atkinson <steven.atkinson@gmail.com>
2020-09-06 23:11:33 -0700

    introduce styles folder, sass-loader and global variables import for all components

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,19 @@
 // vue.config.js
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === 'production'
-      ? '/GreaterGoodsGrocers'
-      : '/GreaterGoodsGrocers',
+  publicPath: process.env.NODE_ENV === 'production' ? '/GreaterGoodsGrocers' : '/GreaterGoodsGrocers',
+
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `@import "@/styles/_variables.scss";`
+      }
+    }
+  },
 
   configureWebpack: {
     devServer: {
       headers: {
-        'Cache-Control':
-          'must-revalidate, no-cache, no-store, no-transform, private, proxy-revalidate, max-age=5',
+        'Cache-Control': 'must-revalidate, no-cache, no-store, no-transform, private, proxy-revalidate, max-age=5',
         'X-Frame-Options': 'DENY',
         'X-XSS-Protection': '1; mode=block',
         'X-Content-Type': 'nosniff'
